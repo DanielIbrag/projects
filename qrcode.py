3db25474e9e63f123efca8b7646f55b642de56e5
Mac <danny.ibragimov1@gmail.com>
2023-08-07 16:13:47 -0400

    analyzing google trends

--- qrcode.py
+++ qrcode.py
@@ -2,10 +2,10 @@ import pyqrcode
 from pyqrcode import QRCode 
   
 # String which represent the QR code 
-s = "https://www.youtube.com/channel/UCeO9hPCfRzqb2yTuAn713Mg"
+s = "https://danielibrag.github.io/"
   
 # Generate QR code 
 url = pyqrcode.create(s) 
   
 # Create and save the png file naming "myqr.png" 
-url.svg("myyoutube.svg", scale = 8) 
+url.svg("mywebsite.svg", scale = 8) 
